dfcca58d69279cf79f16817e753ad5404059611c
Enzo FILLAUDEAU <e.fillaudeau@ip2i.in2p3.fr>
2023-05-06 16:58:02 +0200

    Correc4 modif ref coord .cpp et .h

--- NanoGEN_analysis/NanoGENanalyzer.h
+++ NanoGEN_analysis/NanoGENanalyzer.h
@@ -1013,8 +1013,8 @@ double calculate_cosThetaXStar(TLorentzVector lepton, TLorentzVector qSpec, TLor
    bJet.Boost(InvariantTopBoost);
 
    TVector3 Zdir = qSpec.Vect().Unit();
-   TVector3 bJet = bJet.Vect().Unit();
-   TVector3 Ydir = bJet.Cross(Zdir).Unit();
+   TVector3 bJetUnit = bJet.Vect().Unit();
+   TVector3 Ydir = bJetUnit.Cross(Zdir).Unit();
    TVector3 Xdir = Ydir.Cross(Zdir);
    TVector3 leptonUnitary = lepton.Vect().Unit();
 
@@ -1035,8 +1035,8 @@ double calculate_cosThetaYStar(TLorentzVector lepton, TLorentzVector qSpec, TLor
    bJet.Boost(InvariantTopBoost);
 
    TVector3 Zdir = qSpec.Vect().Unit();
-   TVector3 bJet = bJet.Vect().Unit();
-   TVector3 Ydir = bJet.Cross(Zdir).Unit();
+   TVector3 bJetUnit = bJet.Vect().Unit();
+   TVector3 Ydir = bJetUnit.Cross(Zdir).Unit();
    TVector3 Xdir = Ydir.Cross(Zdir);
    TVector3 leptonUnitary = lepton.Vect().Unit();
 
