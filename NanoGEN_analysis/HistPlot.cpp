0cbead268f2f5c01082b4bd281ab1aad4a9c03db
Enzo FILLAUDEAU <e.fillaudeau@ip2i.in2p3.fr>
2023-05-11 09:52:09 +0200

    stack 2 hist on same canva HistPlot.cpp

--- NanoGEN_analysis/HistPlot.cpp
+++ NanoGEN_analysis/HistPlot.cpp
@@ -3,98 +3,188 @@
 #include <TFile.h>
 #include <TTree.h>
 #include <TCanvas.h>
+#include <THStack.h>
+#include "TLegend.h"
+
 
 void plotHistogramZ()
 {
-    TH1F* h1 = new TH1F("h1", "cosThetaZ*", 30, -1.0, 1.0);
+    TH1F* powheg = new TH1F("powheg", "cosThetaZ*", 30, -1.0, 1.0);
+    TFile* inputP = new TFile("powheg.root", "read");
+    TTree* treeP = (TTree*)inputP->Get("LHE");
 
-    TFile* input = new TFile("test.root", "read");
-    TTree* tree = (TTree*)input->Get("LHE");
+    TH1F* MG5 = new TH1F("MG5", "cosThetaZ*", 30, -1.0, 1.0);
+    TFile* inputM = new TFile("MG5.root", "read");
+    TTree* treeM = (TTree*)inputM->Get("LHE");
 
     float cosThetaZStar;
 
-    tree->SetBranchAddress("cosThetaZStar", &cosThetaZStar);
+    treeP->SetBranchAddress("cosThetaZStar", &cosThetaZStar);
+    treeM->SetBranchAddress("cosThetaZStar", &cosThetaZStar);
 
-    Long64_t num_entries = tree->GetEntries();
+    Long64_t pow_entries = treeP->GetEntries();
 
-    for (Long64_t i = 0; i < num_entries; i++)
+    for (Long64_t i = 0; i < pow_entries; i++)
     {
-        tree->GetEntry(i);
-        h1->Fill(cosThetaZStar);
+        treeP->GetEntry(i);
+        powheg->Fill(cosThetaZStar);
     }
+    powheg->SetLineColor(kRed);
+    powheg->Scale(1.0/powheg->Integral());
 
-    h1->GetXaxis()->SetTitle("cosThetaZ*");
-    h1->GetYaxis()->SetTitle("NbrOfEvent");
+
+    Long64_t MG5_entries = treeM->GetEntries();
+
+    for (Long64_t i = 0; i < MG5_entries; i++)
+    {
+        treeM->GetEntry(i);
+        MG5->Fill(cosThetaZStar);
+    }
+    MG5->SetLineColor(kBlue);
+    MG5->Scale(1.0/MG5->Integral());
+  
+     
+    THStack *histStack = new THStack("histStack", "cosThetaZ*");
+
+   
+    histStack->Add(powheg);
+    histStack->Add(MG5);
+    
 
     TCanvas* canvasZ = new TCanvas("canvasZ", "Histogram Canvas Z", 1200, 800);
-    h1->Draw("E");
+    histStack->Draw("E");
+
+    TLegend *legend = new TLegend(0.8, 0.8, 0.9, 0.9);
+    legend->AddEntry(powheg, "powheg");
+    legend->AddEntry(MG5, "MG5");
+    legend->Draw();
 
     canvasZ->SaveAs("cosThetaZ*.pdf");
 
-    delete h1;
-    input->Close();
+    delete powheg;
+    delete MG5;
+    inputP->Close();
+    inputM->Close();
 }
 
 void plotHistogramX()
 {
-    TH1F* h1 = new TH1F("h1", "cosThetaX*", 30, -1.0, 1.0);
+    TH1F* powheg = new TH1F("powheg", "cosThetaX*", 30, -1.0, 1.0);
+    TFile* inputP = new TFile("powheg.root", "read");
+    TTree* treeP = (TTree*)inputP->Get("LHE");
 
-    TFile* input = new TFile("test.root", "read");
-    TTree* tree = (TTree*)input->Get("LHE");
+    TH1F* MG5 = new TH1F("MG5", "cosThetaX*", 30, -1.0, 1.0);
+    TFile* inputM = new TFile("MG5.root", "read");
+    TTree* treeM = (TTree*)inputM->Get("LHE");
 
     float cosThetaXStar;
 
-    tree->SetBranchAddress("cosThetaXStar", &cosThetaXStar);
+    treeP->SetBranchAddress("cosThetaXStar", &cosThetaXStar);
+    treeM->SetBranchAddress("cosThetaXStar", &cosThetaXStar);
 
-    Long64_t num_entries = tree->GetEntries();
+    Long64_t pow_entries = treeP->GetEntries();
 
-    for (Long64_t i = 0; i < num_entries; i++)
+    for (Long64_t i = 0; i < pow_entries; i++)
     {
-        tree->GetEntry(i);
-        h1->Fill(cosThetaXStar);
+        treeP->GetEntry(i);
+        powheg->Fill(cosThetaXStar);
     }
+    powheg->SetLineColor(kRed);
+    powheg->Scale(1.0/powheg->Integral());
 
-    h1->GetXaxis()->SetTitle("cosThetaX*");
-    h1->GetYaxis()->SetTitle("NbrOfEvent");
+
+    Long64_t MG5_entries = treeM->GetEntries();
+
+    for (Long64_t i = 0; i < MG5_entries; i++)
+    {
+        treeM->GetEntry(i);
+        MG5->Fill(cosThetaXStar);
+    }
+    MG5->SetLineColor(kBlue);
+    MG5->Scale(1.0/MG5->Integral());
+  
+     
+    THStack *histStack = new THStack("histStack", "cosThetaX*");
+
+   
+    histStack->Add(powheg);
+    histStack->Add(MG5);
+    
 
     TCanvas* canvasX = new TCanvas("canvasX", "Histogram Canvas X", 1200, 800);
-    h1->Draw("E");
+    histStack->Draw("E");
+
+    TLegend *legend = new TLegend(0.8, 0.8, 0.9, 0.9);
+    legend->AddEntry(powheg, "powheg");
+    legend->AddEntry(MG5, "MG5");
+    legend->Draw();
 
     canvasX->SaveAs("cosThetaX*.pdf");
 
-    delete h1;
-    input->Close();
+    delete powheg;
+    delete MG5;
+    inputP->Close();
+    inputM->Close();
 }
 
 void plotHistogramY()
 {
-    TH1F* h1 = new TH1F("h1", "cosThetaY*", 30, -1.0, 1.0);
+    TH1F* powheg = new TH1F("powheg", "cosThetaY*", 30, -1.0, 1.0);
+    TFile* inputP = new TFile("powheg.root", "read");
+    TTree* treeP = (TTree*)inputP->Get("LHE");
 
-    TFile* input = new TFile("test.root", "read");
-    TTree* tree = (TTree*)input->Get("LHE");
+    TH1F* MG5 = new TH1F("MG5", "cosThetaY*", 30, -1.0, 1.0);
+    TFile* inputM = new TFile("MG5.root", "read");
+    TTree* treeM = (TTree*)inputM->Get("LHE");
 
     float cosThetaYStar;
 
-    tree->SetBranchAddress("cosThetaYStar", &cosThetaYStar);
+    treeP->SetBranchAddress("cosThetaYStar", &cosThetaYStar);
+    treeM->SetBranchAddress("cosThetaYStar", &cosThetaYStar);
 
-    Long64_t num_entries = tree->GetEntries();
+    Long64_t pow_entries = treeP->GetEntries();
 
-    for (Long64_t i = 0; i < num_entries; i++)
+    for (Long64_t i = 0; i < pow_entries; i++)
     {
-        tree->GetEntry(i);
-        h1->Fill(cosThetaYStar);
+        treeP->GetEntry(i);
+        powheg->Fill(cosThetaYStar);
     }
+    powheg->SetLineColor(kRed);
+    powheg->Scale(1.0/powheg->Integral());
 
-    h1->GetXaxis()->SetTitle("cosThetaY*");
-    h1->GetYaxis()->SetTitle("NbrOfEvent");
+
+    Long64_t MG5_entries = treeM->GetEntries();
+
+    for (Long64_t i = 0; i < MG5_entries; i++)
+    {
+        treeM->GetEntry(i);
+        MG5->Fill(cosThetaYStar);
+    }
+    MG5->SetLineColor(kBlue);
+    MG5->Scale(1.0/MG5->Integral());
+  
+     
+    THStack *histStack = new THStack("histStack", "cosThetaY*");
+
+   
+    histStack->Add(powheg);
+    histStack->Add(MG5);
+    
 
     TCanvas* canvasY = new TCanvas("canvasY", "Histogram Canvas Y", 1200, 800);
-    h1->Draw("E");
+    histStack->Draw("E");
+
+    TLegend *legend = new TLegend(0.8, 0.8, 0.9, 0.9);
+    legend->AddEntry(powheg, "powheg");
+    legend->AddEntry(MG5, "MG5");
+    legend->Draw();
 
     canvasY->SaveAs("cosThetaY*.pdf");
 
-    delete h1;
-    input->Close();
+    delete powheg;
+    delete MG5;
+    inputP->Close();
+    inputM->Close();
 }
 
 int main()
--- NanoGEN_analysis/NanoGENanalyzer.cpp
+++ NanoGEN_analysis/NanoGENanalyzer.cpp
@@ -48,7 +48,7 @@ void NanoGENanalyzer::Loop()
    // const float xSec = 35.8253;
    // sumOf_genWeights_beforeAnalysis = 1.94133e+08 ;
 
-   TFile* fOutput = new TFile("test.root","RECREATE");
+   TFile* fOutput = new TFile("MG5.root","RECREATE");
    const float xSec = 35.4209;
    // sumOf_genWeights_beforeAnalysis = 1.75335e+08;
 
